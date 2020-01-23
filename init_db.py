01922bdc5182798f26a0c8ea9e8be938c079c95e
Maksim Sobin <maxim.sobin@gmail.com>
2020-01-23 23:03:43 +0600

    Add init_db script

---
+++ init_db.py
@@ -0,0 +1,9 @@
+from app.common.session import session
+import app.common.env as env
+
+session.connection().connection.set_isolation_level(0)
+session.execute(f'CREATE DATABASE {env.DB}')
+session.execute(f"CREATE USER {env.DB_USER} WITH PASSWORD '{env.DB_PASSWORD}'")
+session.execute(f'GRANT ALL PRIVILEGES ON DATABASE {env.DB} to {env.DB_USER}')
+session.connection().connection.set_isolation_level(1)
+
