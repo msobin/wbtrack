5f60982298c88eb018f2cdbce19e10e5fab1cd93
Maksim Sobin <maxim.sobin@gmail.com>
2020-02-22 22:56:29 +0600

    Refactoring

--- app/wbbot/bot.py
+++ app/wbbot/bot.py
@@ -7,7 +7,7 @@ from telegram.error import NetworkError
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, ConversationHandler
 
 import common.env as env
-import wbbot.handlers.actions as actions
+import wbbot.handlers.callbacks as actions
 import wbbot.handlers.commands as commands
 import wbbot.handlers.messages as messages
 import wbbot.misc.jobs as jobs
@@ -15,11 +15,11 @@ import wbbot.handlers.menu as menu
 
 
 def main():
-    # logging.basicConfig(level=logging.INFO,
-    #                     handlers=[TimedRotatingFileHandler(filename=env.LOG_DIR + '/bot.log', when='midnight',
-    #                                                        backupCount=7)],
-    #                     format='%(asctime)s - %(levelname)s - %(message)s')
-
+    logging.basicConfig(level=logging.INFO,
+                        handlers=[TimedRotatingFileHandler(filename=env.LOG_DIR + '/bot.log', when='midnight',
+                                                           backupCount=7)],
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    #
     # logging.basicConfig()
     # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
@@ -34,10 +34,10 @@ def main():
     dispatcher.add_handler(CommandHandler('brands', commands.command_brands))
     dispatcher.add_handler(CommandHandler('catalog', commands.command_catalog))
     dispatcher.add_handler(CommandHandler('logout', commands.command_logout))
-    dispatcher.add_handler(CommandHandler('menu', menu.start))
 
-    dispatcher.add_handler(MessageHandler(Filters.reply, messages.message_search))
     dispatcher.add_handler(MessageHandler(Filters.regex(env.PRODUCT_REGEXP), messages.message_add_product))
+    dispatcher.add_handler(MessageHandler(Filters.text, menu.menu_item_select))
+    dispatcher.add_handler(MessageHandler(Filters.reply, messages.message_search))
 
     dispatcher.add_handler(CallbackQueryHandler(actions.inline_callback))
 
--- app/wbbot/handlers/menu.py
+++ app/wbbot/handlers/menu.py
@@ -1,7 +1,81 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup
+
+import wbbot.actions.common as actions
+
+MENU_ITEM_HOME = '🏠 Главное меню'
+MENU_ITEM_BACK = '🔙 Назад'
+
+menu = {
+    '🛍️ Товары': {
+        '🔍 Найти': 'action_search',
+        '📃 Список': 'action_list',
+        '🗂️ Каталог': 'action_catalog',
+        '👓 Брэнды': 'action_brands',
+
+    },
+    '⚙️ Настройки': 'action_stub',
+}
 
 
-def start(update, context):
-    reply_keyboard = [['Boy'], ['Girl'], ['Other']]
+def menu_item_select(update, context):
+    item = update.message.text
+    path = context.user_data.get('menu_path')
+
+    if item == MENU_ITEM_HOME:
+        path = None
+    elif item == MENU_ITEM_BACK:
+        path = '.'.join(path.split('.')[:-1])
+    else:
+        path = '.'.join([path, item]) if path else item
+
+    sub_menu = get_sub_menu(path)
+
+    if type(sub_menu) is dict:
+        reply_keyboard = get_reply_keyboard(path)
+    else:
+        return globals()[sub_menu](update, context)
+
+    context.user_data['menu_path'] = path
     update.message.reply_text('test', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False))
 
+
+def get_reply_keyboard(path):
+    sub_menu = get_sub_menu(path)
+
+    reply_keyboard = [[key] for key in sub_menu.keys()]
+
+    if path:
+        reply_keyboard.append([MENU_ITEM_BACK])
+        reply_keyboard.append([MENU_ITEM_HOME])
+
+    return reply_keyboard
+
+
+def get_sub_menu(path):
+    sub_menu = menu
+
+    if path:
+        for key in path.split('.'):
+            sub_menu = sub_menu.get(key)
+
+    return sub_menu
+
+
+def action_search(update, context):
+    return actions.products_search(update, context)
+
+
+def action_list(update, context):
+    return actions.products_list(update, context)
+
+
+def action_brands(update, context):
+    return actions.brands_list(update, context)
+
+
+def action_catalog(update, context):
+    return actions.products_catalog(update, context)
+
+
+def action_stub():
+    pass
